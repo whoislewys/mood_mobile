f87d5cd4047ef8d6a67ecf34a36c545e649cd91a
Sam Rondinelli <sam.rondinelli3@gmail.com>
2017-02-12 16:40:21 -0700

    Added android support

--- index.ios.js
+++ index.ios.js
@@ -1,32 +1,6 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 
-import Playscreen from './components/playscreen';
-import Images from '@assets/images';
-import Background from './components/background'
+import Main from './main.js';
 
-var Mood = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Background>
-          <Playscreen />
-        </Background>
-      </View>
-    );
-  }
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-});
-
-AppRegistry.registerComponent('moodmobile', () => Mood);
+AppRegistry.registerComponent('moodmobile', () => Main);
---
+++ main.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+import Playscreen from './components/playscreen';
+import Images from '@assets/images';
+import Background from './components/background'
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Background>
+          <Playscreen />
+        </Background>
+      </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
