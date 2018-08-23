7f702a940a9a967f2d3ccd9d70b4336995aece85
Sam Rondinelli <sam.rondinelli3@gmail.com>
2018-08-22 21:20:43 -0700

    add all

--- src/navigation/app-navigator.js
+++ src/navigation/app-navigator.js
@@ -3,21 +3,19 @@ import { createStackNavigator } from 'react-navigation';
 import { Easing, Animated } from 'react-native';
 
 import PlayScreen from '../screens/play';
-import MoodScreen from '../screens/mood'
+import MoodScreen from '../screens/mood';
 import SplashScreen from '../screens/splash';
 import SettingsScreen from '../screens/settings';
 import ErrorScreen from '../screens/error';
 
-const map = (SomeComponent) => {
-  return class SomeClass extends React.Component {
+const map = SomeComponent => class SomeClass extends React.Component {
     render = () => {
       const screenProps = this.props.screenProps;
       delete this.props.screenProps;
-      const {navigation: {state: {params}}} = this.props
-      return <SomeComponent {...params} {...this.props} {...screenProps} />
+      const { navigation: { state: { params } } } = this.props;
+      return <SomeComponent {...params} {...this.props} {...screenProps} />;
     }
-  }
-}
+};
 
 export default createStackNavigator({
   Splash: { screen: map(SplashScreen) },
@@ -25,7 +23,7 @@ export default createStackNavigator({
   Play: { screen: map(PlayScreen) },
   Settings: { screen: map(SettingsScreen) },
   Error: { screen: map(ErrorScreen) },
-},{
+}, {
   headerMode: 'none',
   navigationOptions: {
     gesturesEnabled: false,
@@ -36,7 +34,7 @@ export default createStackNavigator({
       easing: Easing.out(Easing.poly(4)),
       timing: Animated.timing,
     },
-    screenInterpolator: sceneProps => {
+    screenInterpolator: (sceneProps) => {
       const { layout, position, scene } = sceneProps;
       const { index } = scene;
 
