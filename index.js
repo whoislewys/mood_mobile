2f395efe4a4baf73d21318a6a7968684ff8e339c
Sam Rondinelli <sam.rondinelli3@gmail.com>
2018-09-21 17:12:48 -0700

    Nuked everything

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('moodmobile', () => App);
+AppRegistry.registerComponent('mood_mobile', () => App);
