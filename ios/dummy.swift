76fdfe8c5f2af979086768a15fb711bb1ae70fae
whoislewys <whoislewys@gmail.com>
2018-11-04 12:30:51 -0700

    add swift file for react-native-track-player to work correctly

---
+++ ios/dummy.swift
@@ -0,0 +1,12 @@
+//
+//  dummy.swift
+//  mood_mobile
+//
+//  Created by lewys on 11/4/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+//  *****************************************
+//  USED TO BRIDGE REACT-NATIVE-AUDIO-TOOLKIT
+//  *****************************************
+
+import Foundation
