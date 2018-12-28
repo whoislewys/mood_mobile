14666842ebacb3b6c08f6328ee8c7afb52e33cc2
whoislewys <whoislewys@gmail.com>
2018-12-27 19:23:48 -0700

    expose the score globally thru redux

--- src/redux/modules/queue.js
+++ src/redux/modules/queue.js
@@ -5,15 +5,21 @@ const LOAD_SONGS = 'queue/LOAD';
 const LOAD_SONGS_SUCCESS = 'queue/LOAD_SUCCESS';
 const LOAD_SONGS_FAIL = 'queue/LOAD_FAIL';
 
+const UPDATE_CURRENT_TRACK = 'queue/UPDATE_CURRENT_TRACK';
+
 const PLAYBACK_STATE = 'playback/STATE';
 const PLAYBACK_TRACK = 'playback/TRACK';
 
+const UPDATE_SCORE = 'UPDATE_SCORE';
+
 const initialState = {
   loading: false,
+  currentTrack: {},
   errors: null,
   queue: [],
   playback: null,
   track: null,
+  currentScore: 0,
 };
 
 export function loadSongData(list) {
@@ -25,6 +31,7 @@ export function loadSongData(list) {
     album: t.album_name,
     artwork: t.art_url,
     mood_id: t.mood_id,
+    score: 0,
   })), 'artist');
 }
 
@@ -33,15 +40,28 @@ export default function reducer(state = initialState, action = {}) {
     case LOAD_SONGS:
       return { ...state, loading: true, queue: [] };
     case LOAD_SONGS_SUCCESS:
+      // load songs for mood
       let songs = null;
       songs = loadSongData(action.payload.data);
-      return { ...state, loading: false, queue: songs };
+      // calculate current track
+      let track = songs.find(e => (e.id === state.track));
+      if (track === undefined) track = songs[0]; // This is gross, I promise I'll fix it
+      return {
+        ...state,
+        loading: false,
+        queue: songs,
+        currentTrack: track,
+      };
     case LOAD_SONGS_FAIL:
       return {
         ...state,
         loading: false,
         error: 'Error while loading songs.',
       };
+    case UPDATE_CURRENT_TRACK:
+      let newTrack = state.queue.find(e => (e.id === state.track));
+      if (newTrack === undefined) newTrack = songs[0]; // This is gross, I promise I'll fix it
+      return { ...state, currentTrack: newTrack };
     case PLAYBACK_STATE:
       return {
         ...state,
@@ -52,6 +72,13 @@ export default function reducer(state = initialState, action = {}) {
         ...state,
         track: action.track,
       };
+    case UPDATE_SCORE:
+      // let updatedCurrentTrack = state.currentTrack;
+      // updatedCurrentTrack.score = action.newScore;
+      // option 1: call api here with scoreDelta 1
+      // return { ...state, currentTrack: updatedCurrentTrack, currentScore: action.newScore };
+      // ^ throwing frozen object error, apparently currentTrack is immutable ^
+      return { ...state, currentScore: action.newScore };
     default:
       return state;
   }
@@ -82,5 +109,19 @@ export function playbackTrack(track) {
   return {
     type: PLAYBACK_TRACK,
     track,
+  };
+}
+
+export function updateScore(newScore) {
+  console.log('new score: ', newScore);
+  return {
+    type: UPDATE_SCORE,
+    newScore,
+  };
+}
+
+export function updateCurrentTrack() {
+  return {
+    type: UPDATE_CURRENT_TRACK,
   };
 }
