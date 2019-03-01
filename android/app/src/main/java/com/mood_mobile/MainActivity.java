3bf2bc1af7d79faa92677d64125673715e9cdc18
whoislewys <whoislewys@gmail.com>
2019-02-28 17:23:11 -0700

    add android support commits

--- android/app/src/main/java/com/mood_mobile/MainActivity.java
+++ android/app/src/main/java/com/mood_mobile/MainActivity.java
@@ -1,14 +1,30 @@
 package com.mood_mobile;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
+import io.branch.rnbranch.*;
+import android.content.Intent;
+// react-native-splash-screen < 0.3.1
+// import com.cboy.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
+   @Override
+   protected void onStart() {
+        super.onStart();
+        RNBranchModule.initSession(getIntent().getData(), this);
+   }
+   @Override
+   public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+   }
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
     protected String getMainComponentName() {
         return "mood_mobile";
     }
