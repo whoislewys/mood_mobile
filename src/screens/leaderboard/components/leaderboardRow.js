9a9c7b519b0e741b7ca3f29d191cfc128f366933
whoislewys <whoislewys@gmail.com>
2019-03-03 01:13:23 -0700

    refactor queue module to use curTrackIndex

--- src/screens/leaderboard/components/leaderboardRow.js
+++ src/screens/leaderboard/components/leaderboardRow.js
@@ -106,7 +106,7 @@ const LeaderboardRow = ({
 
   return (
     <TouchableOpacity style={styles.rowBackground} onPress={
-      () => _handleLeaderboardRowPress(leaderboardSong)
+      () => _handleLeaderboardRowPress(index)
     }>
       <Text style={styles.rank}>{index + 1}</Text>
       <Image style={styles.albumArt} source={{ uri: artwork }}/>
