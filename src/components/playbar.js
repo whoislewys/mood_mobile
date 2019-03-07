4a6578fcc6abb115cb374b01c05bdf48351a0329
whoislewys <whoislewys@gmail.com>
2019-03-07 14:07:39 -0700

    add playbar back baby!!!!!!

---
+++ src/components/playbar.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Text,
+} from 'react-native';
+import Images from '@assets/images';
+import { dimensions } from '../assets/styles';
+
+const { width } = dimensions;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    flex: 1,
+  },
+  albumArt: {
+    resizeMode: 'cover',
+    flex: 1,
+  },
+  arrow: {
+    resizeMode: 'contain',
+    flex: 1,
+    marginLeft: 24,
+    width: 22,
+    height: 11,
+    opacity: 0.8,
+  },
+  art: {
+    flex: 20,
+    justifyContent: 'flex-start',
+  },
+  info: {
+    flex: 88,
+  },
+  subInfo: {
+    flexDirection: 'row',
+  },
+  controls: {
+    flex: 20,
+  },
+  albumInfoText: {
+    flexDirection: 'row',
+    paddingLeft: width * 0.02,
+    width: 280,
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: 'Roboto',
+    fontWeight: '300',
+    textAlign: 'center',
+  },
+  albumInfoSubText: {
+    color: '#fff',
+    fontSize: 14,
+    fontFamily: 'Roboto',
+    fontWeight: '300',
+  },
+  playPauseButton: {
+    width: 30,
+    height: 30,
+    backgroundColor: 'transparent',
+  },
+});
+
+export default class PlayBar extends React.Component {
+  playButton = () => {
+    if (this.props.playing) {
+      return (
+        <TouchableOpacity onPress={this.props.handlePlayPress}>
+          <Image source={Images.pauseButton} style={styles.playPauseButton} />
+        </TouchableOpacity>
+      );
+    }
+    return (
+      <TouchableOpacity onPress={this.props.handlePlayPress}>
+        <Image source={Images.playButton} style={styles.playPauseButton} />
+      </TouchableOpacity>
+    );
+  }
+
+  albumArt = () => (
+    <Image source={Images.arrowDown} style={styles.arrow} />
+  )
+
+  render = () => {
+    const { track } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.art} onPress={this.props.go}>
+          {this.albumArt()}
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.props.go} style={styles.info}>
+          <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+            <Text
+              style={[styles.albumInfoText, {
+                maxWidth: width * 0.61,
+                textAlign: 'center',
+              }]}
+              numberOfLines={1}
+              ellipsizeMode="tail"
+            >
+              { track.name }
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.controls}>
+          {this.playButton()}
+        </View>
+      </View>
+    );
+  }
+}
