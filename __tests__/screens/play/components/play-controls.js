77ae4fc5f5459835de15ca530f4400d7737065e8
whoislewys <whoislewys@gmail.com>
2019-04-03 20:39:37 -0700

    clean up playbutton test

--- __tests__/screens/play/components/play-controls.js
+++ __tests__/screens/play/components/play-controls.js
@@ -2,37 +2,10 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import PlayControls from '../../../../src/screens/play/components/play-controls';
-import {initialState} from '../../../../src/redux/modules/queue';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('PlayControls Component', () => {
-  let mockPlayControlProps;
-
-  // beforeAll(() => {
-  //   const track1 = {
-  //     id: 69,
-  //     file: 'https://mood-music-api.herokuapp.com//rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBblVCIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--e2afd16bbd89ed4503d3f60a362a6ddd263992fc/songs167.mp3',
-  //     name: 'Coffee (prod. KRVSH)',
-  //     artist: 'Isaac Von',
-  //     album_name: 'while(1<2)',
-  //     art_url: 'https://i1.sndcdn.com/artworks-000232798771-b7p886-t500x500.jpg',
-  //     mood_id: 3,
-  //     stars: 100,
-  //     rank: 1,
-  //   };
-  //   mockPlayControlProps = {
-  //     logEvent: jest.fn(),
-  //     skipForward: jest.fn(),
-  //     skipBack: jest.fn(),
-  //     playing: true,
-  //     handlePlayPress: jest.fn(),
-  //     loading: false,
-  //     currentTrack: track1,
-  //   };
-  // });
-
-
   it('shallow renders', () => {
     const wrapper = shallow(<PlayControls />);
     const playControlsInstance = wrapper.instance();
@@ -41,16 +14,22 @@ describe('PlayControls Component', () => {
   });
 
   it('handles play presses', () => {
+    // NOTE: the playbutton should really be it's own component. right now it's copypasta'd a couple different places
+    // but this is an example of how you would exercise onPress functionality somewhere
+
     // 1. mock the playpress function
     const mockHandlePlayPress = jest.fn();
+
     // 2. pass mock fn into the component
     const wrapper = shallow(<PlayControls handlePlayPress={mockHandlePlayPress} />);
 
-    // 3. reach down and find the playbutton
+    // 3. reach down and find the PlayButton
     const playButton = wrapper.find({ testId: 'play-button' });
+
+    // 4. fake pressing the play button
     playButton.props().onPress();
 
-    // 4. assert the proper function was called
+    // 5. assert the proper function was called
     expect(mockHandlePlayPress).toHaveBeenCalledTimes(1);
   });
 });
