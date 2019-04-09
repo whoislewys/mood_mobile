4f5500648be0b857a07fd060818bd4d06b3d0d93
whoislewys <whoislewys@gmail.com>
2019-04-09 10:55:01 -0700

    (working) playlist detail screen

---
+++ src/screens/playlistDetail/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+const PlaylistDetailScreen = props => (
+  <View style={styles.container}>
+    <Text>shittites</Text>
+    <TouchableOpacity style={{backgroundColor: 'red', height: 100, width: 100, alignSelf: 'center'}} onPress={() => props.navigation.goBack()} />
+  </View>
+);
+
+export default PlaylistDetailScreen;
