a312afb88c80ef0f08377ab830ee15de95936429
whoislewys <whoislewys@gmail.com>
2019-04-09 11:12:15 -0700

    clean up images

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,22 +1,27 @@
 /* eslint global-require: 0 */
 const images = {
+  // Common
   arrowDown: require('./img/selected-down-chevron.png'),
   arrowLeft: require('./img/selected-left-chevron.png'),
   bgOverlay: require('./img/background-overlay.png'),
-  playButton: require('./img/round-play-2x.png'),
-  pauseButton: require('./img/round-pause-2x.png'),
-  skip: require('./img/selected-round-skip_previous.png'),
-  star: require('./img/selected-star-rounded.png'),
-  share: require('./img/selected-share-arrow.png'),
-  shareOutline: require('./img/share-arrow-outline.png'),
-  sliderButton: require('./img/slider-button.png'),
-  gradientHeading: require('./img/gradient-heading.png'),
   bugIcon: require('./img/round-bug_report.png'),
   settingsGear: require('./img/round-settings.png'),
+
+  // Leaderboard
   leaderboardStar: require('./img/round-stars-1.png'),
-  starOutline: require('./img/star-rounded-outline.png'),
   moodLogo: require('./img/Color-Mood-Logo.png'),
-  // Navigation Bar
+
+  // Media Player
+  pauseButton: require('./img/round-pause-2x.png'),
+  playButton: require('./img/round-play-2x.png'),
+  share: require('./img/selected-share-arrow.png'),
+  shareOutline: require('./img/share-arrow-outline.png'),
+  skip: require('./img/selected-round-skip_previous.png'),
+  star: require('./img/selected-star-rounded.png'),
+  starOutline: require('./img/star-rounded-outline.png'),
+  sliderButton: require('./img/slider-button.png'),
+
+  // Nav Bar
   discover: require('./img/selected-round-discover.png'),
   home: require('./img/selected-home-icon.png'),
   player: require('./img/selected-round-media-player.png'),
@@ -24,7 +29,8 @@ const images = {
   navPauseButton: require('./img/round-pause_circle_outline-2x.png'),
   leaderboard: require('./img/selected-round-format-leaderboard.png'),
   events: require('./img/selected-round-events.png'),
-  // SettingsScreen
+
+  // Settings Screen
   explicitIcon: require('./img/explicit-icon.png'),
   addEventButton: require('./img/add-event-button.png'),
   settingsButton: require('./img/settings-button.png'),
