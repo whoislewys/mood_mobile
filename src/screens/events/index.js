0f5760f47e6cea86a606cf804cfa393faba1ef71
whoislewys <whoislewys@gmail.com>
2019-05-07 21:24:44 -0700

    width fix on add event button

--- src/screens/events/index.js
+++ src/screens/events/index.js
@@ -60,7 +60,11 @@ class EventsScreen extends Component {
     <View style={styles.background}>
       <MoodLeftHeader title='Phx Events'>
         {/*<GradientButton text='ADD EVENT' onPress={() => Linking.openURL(ADD_EVENT_URL)} width={90} />*/}
-        <GradientButton text='ADD EVENT' onPress={() => this.props.navigation.navigate('EventsForm')} />
+        <GradientButton
+          text='ADD EVENT'
+          onPress={() => this.props.navigation.navigate('EventsForm')}
+          width={90}
+        />
       </MoodLeftHeader>
       <View style={styles.eventsContainer}>
         {this.getEvents()}
