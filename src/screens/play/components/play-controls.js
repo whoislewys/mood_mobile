75f7b722f1dbdab0fff7e2115b1a8eb1482b2cc7
whoislewys <whoislewys@gmail.com>
2019-06-28 19:38:16 -0700

    [branch-custom-domain-name] i think this does it for android (+fix type in ios)

--- src/screens/play/components/play-controls.js
+++ src/screens/play/components/play-controls.js
@@ -136,7 +136,7 @@ export default class PlayControls extends Component {
     const linkProperties = { feature: 'share', channel: 'RNApp' };
     const controlParams = {
       $desktop_url: 'http://www.moodindustries.com',
-      $ios_url: 'https://moodmusic.app.link/ZIFgV4QdLS',
+      $ios_url: 'https://app.moodindustries.com/ZIFgV4QdLS',
     };
     const { url } = await this._buo.generateShortUrl(linkProperties, controlParams);
     await Share.share({ message: `Check out this bop on mood! ${url}`, title: 'I have some new music for you!' });
