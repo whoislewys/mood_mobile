908f6d62ded4389a75e2183e71f1ca2ce1518021
whoislewys <whoislewys@gmail.com>
2019-07-21 12:45:05 -0700

    [trackplayer-info] current track sync middleware

---
+++ src/redux/middlewares/update-current-track.js
@@ -0,0 +1,39 @@
+import TrackPlayer from 'react-native-track-player';
+import {
+  PLAYBACK_TRACK,
+  SET_CUR_TRACK,
+} from '../constants';
+
+export const updateCurrentTrack = store => next => (action) => {
+  // This middleware waits a bit after getting a track-change event, then makes sure the store's current track is in sync with the TrackPlayer
+  next(action);
+  if (action.type === PLAYBACK_TRACK) {
+    let updateCurTrackTimer;
+    let updateCurrentTrackFunc = async () => {
+      try {
+        // Check if the curtrack coming from track-change event & the curtrack coming from the TrackPlayer are different
+        // If they are, update the store with the TrackPlayer's curTrack
+        const curTrack = action.newCurTrack;
+        const tpCurTrack = await TrackPlayer.getTrack(await TrackPlayer.getCurrentTrack());
+
+        if (tpCurTrack.id !== curTrack.id) {
+          const storeQueue = store.getState().queue.queue;
+          store.dispatch({
+            type: SET_CUR_TRACK,
+            newCurTrack: tpCurTrack,
+            newCurTrackIndex: storeQueue.findIndex(findTrack => findTrack.id === tpCurTrack.id),
+          });
+        }
+
+        if (updateCurTrackTimer != null) {
+          // once the timer fires and calls this func, clean up that timer as final step
+          // if you need to spin up another check, do it based on a lock screen open event
+          updateCurTrackTimer.clearTimeout();
+        }
+      } catch (e) {}
+    };
+
+    updateCurTrackTimer = setTimeout(updateCurrentTrackFunc, 2000);
+  }
+};
+
