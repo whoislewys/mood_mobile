a2886d86fbad41734e032f01cb78ba1913c2f304
whoislewys <whoislewys@gmail.com>
2019-07-21 15:14:32 -0700

    [trackplayer-info] little borderradius fixes

--- src/screens/play/components/album-art-carousel-item.js
+++ src/screens/play/components/album-art-carousel-item.js
@@ -9,8 +9,8 @@ import { dimensions } from '../../../assets/styles';
 const styles = StyleSheet.create({
   albumContainer: {
     flex: 1,
-    alignSelf: 'center',
     elevation: 10,
+    justifyContent: 'center',
     shadowColor: 'black',
     shadowRadius: 4,
     shadowOpacity: 0.16,
@@ -19,15 +19,15 @@ const styles = StyleSheet.create({
       height: 4,
     },
   },
-  albumArt: {
-    flex: 1,
-    // give this image the same height & width so
-    // borderRadius will be applied
+  albumArtRadiusContainer: {
+    // extra container around image to reliably apply borderRadius cross platform
     height: 0.903 * dimensions.width,
     width: 0.903 * dimensions.width,
-    resizeMode: 'contain',
-    borderRadius: 4,
+    borderRadius: 5,
     overflow: 'hidden',
+  },
+  albumArt: {
+    flex: 1,
   },
 });
 
@@ -36,10 +36,12 @@ export default class AlbumArtCarouselItem extends Component {
   render = () => {
     return (
       <View style={styles.albumContainer}>
-        <Image
-          style={styles.albumArt}
-          source={{ uri: this.props.artwork }}
-        />
+        <View style={styles.albumArtRadiusContainer}>
+          <Image
+            style={styles.albumArt}
+            source={{ uri: this.props.artwork }}
+          />
+        </View>
       </View>
     );
   }
