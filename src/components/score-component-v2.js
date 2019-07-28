d93770c96b1d15f6e2bb3fb3d4457ade4b29433e
whoislewys <whoislewys@gmail.com>
2019-07-28 15:21:49 -0700

    [star-to-heart] add heart component

---
+++ src/components/score-component-v2.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from 'react-native';
+import Images from '@assets/images';
+import { incrementScore } from '../redux/modules/score';
+
+const styles = StyleSheet.create({
+  starButton: {
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  star: {
+    resizeMode: 'contain',
+    height: 30,
+    width: 30,
+    tintColor: '#fff',
+  },
+});
+
+export class HeartButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxCount: 1,
+    };
+  }
+
+  like = () => {
+    if (!this.props.userIsLoggedIn) {
+      this.props.navigation.navigate('Login');
+      return;
+    }
+    // start from currentScore + 1 to not show 0 hearts
+    const { maxCount } = this.state;
+    const newScore = this.props.currentScore + 1;
+    if (newScore <= maxCount) {
+      this.props.incrementScore();
+    }
+  };
+
+  render = () => {
+    // extraStyles can be passed down to adjust tint color
+    // (white on playscreen, black on homescreen)
+    const { extraStyles } = this.props;
+
+    const heartIcon = (this.props.currentScore < 1)
+      ? <Image source={Images.heartOutline} style={[styles.star, extraStyles]} />
+      : <Image source={Images.heart} style={[styles.star, extraStyles]} />;
+
+    return (
+      <View testID='StarTouchableContainer'>
+        <TouchableOpacity
+          accessible={false}
+          testID='StartTouchable'
+          onPress={this.like}
+          activeOpacity={0.7}
+          style={styles.starButton}
+        >
+          {heartIcon}
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  currentScore: state.score.currentScore,
+  scoreDelta: state.score.scoreDelta,
+  userIsLoggedIn: state.auth.userIsLoggedIn,
+});
+
+const mapDispatchToProps = {
+  incrementScore,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeartButton);
